cee15f45a716bb6b7258894727c36c630179ae20
devatha k <k.n.devs@gmail.com>
2016-06-25 19:49:49 -0400

    added enter event

--- event/script.js
+++ event/script.js
@@ -1,5 +1,4 @@
 console.log("connected")
-document.getElementById("taylor").addEventListener("click", hiddelton)
 
 function hiddelton(){
 	console.log("I got picked by taylor on her jet");
@@ -7,6 +6,27 @@ function hiddelton(){
 	var div = document.createElement('div');
   div.setAttribute('id', 'just-an-id')
   document.body.appendChild(div);
-  
+
   document.getElementById("just-an-id").innerHTML="Hidd: mom meet my girlfriend Taylor"
 }
+
+document.getElementById("taylor").addEventListener("click", hiddelton)
+///////////////////////////////
+
+
+// for enter key///////////////
+document.getElementById("photograpger-name").addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+    		var photographerName = document.getElementById("photograpger-name").value
+    console.log(photographerName)
+    if (photographerName=="Bill Cunningham"){
+    	console.log("You are missed.")
+    }
+    else
+    {
+    	console.log("Take a Hint: Minimalism makes people humble.")
+    }
+      
+    }
+})
