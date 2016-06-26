4854554850182b5eea26521c6a854996b4766fbd
devatha k <k.n.devs@gmail.com>
2016-06-26 14:30:17 -0400

    condtructor

--- objectified/scripts.js
+++ objectified/scripts.js
@@ -10,6 +10,22 @@ var empty = [];
 
 for (var prop in topList) {
   console.log(prop + " : " + topList[prop]);
+  console.log(topList.one);
   empty.push(topList[prop])
   console.log(empty);
 }
+
+//////////object constructor////
+
+function newTrack(name, year, album, geners){
+	this.artistname  = name
+	this.albumyear  = year
+	this.songalbum  = album
+	this.songgeners  = geners
+}
+
+var popArtistOne = new newTrack("Rihanna", 2016, "Anti", "pop");
+console.log(popArtistOne )
+console.log(popArtistOne.artistname)
+
+
