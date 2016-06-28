95207a6875030066549bc7775ad49f63ee74702c
devatha k <k.n.devs@gmail.com>
2016-06-28 14:37:26 -0400

    sorting the unorganized tabs

--- datetime/scripts.js
+++ datetime/scripts.js
@@ -24,60 +24,60 @@ console.log(date)
 ////// clear time out, will help you clear the time that you had set. 
 // some simple examples of how to settime functions
 
-		function patSummit (){
-			function shesays (){
-				console.log("there are times you can win a game by the margin of a single good thought or positive word or an action- Pat Summit")
-			}
+	function patSummit (){
+		function shesays (){
+			console.log("there are times you can win a game by the margin of a single good thought or positive word or an action- Pat Summit")
+		}
 			setTimeout(shesays, 2000)
-		}
+	}
 
-		patSummit();
+	patSummit();
 
-		//////////////////////
+	//////////////////////
 
-		var hawaii = function somefunction(){
-		  console.log("marooned island")
-		}
-		hawaii();
+	var hawaii = function somefunction(){
+		console.log("marooned island")
+	}
+	hawaii();
 
-	  //////////////////////
-
-
-		var rome = setTimeout(function(){
-			console.log("I have a special visitor today: taylor and hidd")
-		}, 3000)
-
-		//////////////////////
+	//////////////////////
 
 
-		var amber = setTimeout(perfectlyPreserved, 4000)
-		function perfectlyPreserved (){
-				console.log("i have clearTimeout on me(see below), so I'll never be called to join the party. check you console, if you don't believe.")
-		}
+	var rome = setTimeout(function(){
+		console.log("I have a special visitor today: taylor and hidd")
+	}, 3000)
+
+	//////////////////////
 
 
-    /////// to stop the timeout use clearTimeout 
+	var amber = setTimeout(perfectlyPreserved, 4000)
+	function perfectlyPreserved (){
+		console.log("i have clearTimeout on me(see below), so I'll never be called to join the party. check you console, if you don't believe.")
+	}
 
-    clearTimeout(amber)
+
+  /////// to stop the timeout use clearTimeout 
+
+  clearTimeout(amber)
 
 
 ////////////// setinterval///////////////
 
-		function myFunction() {
-		  setInterval(function(){ alert("the chorus"); }, 4000);
-		}
+	function myFunction() {
+		setInterval(function(){ alert("the chorus"); }, 4000);
+	}
 
-		// myFunction() 
+	// myFunction() 
 
-		////////// 
-		var year = setInterval(seasons, 4000)
-			function seasons(){
-			  console.log("The only season needed is an endless Summer.")
-		}
+	////////// 
+	var year = setInterval(seasons, 4000)
+	function seasons(){
+		console.log("The only season needed is an endless Summer.")
+	}
 
-		/////// to stop the timeout use clearTimeout 
+	/////// to stop the timeout use clearTimeout 
 
-		clearInterval(year)
+	clearInterval(year)
 
 
 
