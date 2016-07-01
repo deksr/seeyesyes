93c5f6d678eb7e42ba6ce2360bb3bacaaae5f60a
devatha k <k.n.devs@gmail.com>
2016-07-01 00:41:51 -0400

    data binding

--- d3/scripts.js
+++ d3/scripts.js
@@ -3,7 +3,14 @@ console.log("connect");
 d3.select("body").append("p").text("new paragraph")
 
 // can also be written like this
-var body = d3.select("body");
-var p = body.append("p");
-p.text("newparagraph")
+// var body = d3.select("body");
+// var p = body.append("p");
+// p.text("newparagraph")
+
+/////////binding data///////
+// to bind we need the data and a dom element selection with few more d3 methods like .data()... 
+var dataset = [ 5, 10, 15, 20, 56 ];
+d3.select("body").selectAll("p").data(dataset).enter().append("p").text("new paragraph")
+console.log(d3.selectAll("p"));
+
 
