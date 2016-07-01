0a3e4a2e700c87c5e579d7b13179a98ee030deec
devatha k <k.n.devs@gmail.com>
2016-07-01 00:42:38 -0400

    displaying data

--- d3/scripts.js
+++ d3/scripts.js
@@ -9,8 +9,14 @@ d3.select("body").append("p").text("new paragraph")
 
 /////////binding data///////
 // to bind we need the data and a dom element selection with few more d3 methods like .data()... 
+// var dataset = [ 5, 10, 15, 20, 56 ];
+// d3.select("body").selectAll("p").data(dataset).enter().append("p").text("new paragraph")
+// console.log(d3.selectAll("p"));
+
+/////////// displaying data ////////////
 var dataset = [ 5, 10, 15, 20, 56 ];
-d3.select("body").selectAll("p").data(dataset).enter().append("p").text("new paragraph")
-console.log(d3.selectAll("p"));
+d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d){
+	return "number of people in the movie theater " + d;
+});
 
 
