474009619d26b367ec755672016d4c3a464f9c3a
devatha k <k.n.devs@gmail.com>
2016-07-01 00:44:11 -0400

    syle with condition

--- d3/scripts.js
+++ d3/scripts.js
@@ -21,9 +21,23 @@ d3.select("body").append("p").text("new paragraph")
 
 
 ///////// displaying the css//////////
+// var dataset = [ 5, 10, 15, 20, 56 ];
+// d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d){
+// 	return "number of people in the movie theater " + d;
+// }).style("color", "red");
+
+// or
+
 var dataset = [ 5, 10, 15, 20, 56 ];
 d3.select("body").selectAll("p").data(dataset).enter().append("p").text(function(d){
 	return "number of people in the movie theater " + d;
-}).style("color", "red");
+}).style("color", function(d) {
+    if (d > 15) {   //Threshold of 15
+        return "red";
+    } else {
+        return "black";
+    }
+});
+
 
 
