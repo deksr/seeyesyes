8d5e91e406899cbcd057dc02b7e846e6ad43af77
devatha k <k.n.devs@gmail.com>
2016-07-01 00:40:57 -0400

    store in variable

--- d3/scripts.js
+++ d3/scripts.js
@@ -2,17 +2,8 @@ console.log("connect");
 
 d3.select("body").append("p").text("new paragraph")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// can also be written like this
+var body = d3.select("body");
+var p = body.append("p");
+p.text("newparagraph")
 
