f70daa4c7f40f878b86234d5fe75004fa58daf40
devatha k <k.n.devs@gmail.com>
2016-07-08 02:46:01 -0400

    traverse

--- 007-jQ/traversal/scripts.js
+++ 007-jQ/traversal/scripts.js
@@ -2,8 +2,86 @@ console.log("traverse-connect");
 
 $( document ).ready(function() {
   console.log( "ready!" );
-  // $( "div" ).children().first().css( "color", "red");
-  $( "li" ).each(function( index ) {
-    console.log( index + ": " + $( this ).text() );
-  });
-});
+
+  // Interesting life of dom elements
+  //////////////////////////////////////
+
+  $(".music-gene").children().css( "color", "green");
+
+  $(".riri").closest("p").css( "color", "pink");
+
+
+
+  // $( "div" ).children().first().css( "color", "green");
+
+  // $( "li" ).each(function( index ) {
+  //   console.log( index + ": " + $( this ).text() );
+  // });
+
+  // $( "a" ).parentsUntil('.columns').css( "color", "green");
+
+
+ // selects sibling elements except the specified element $(".jonas"). If two p tags have .jonas then both are selected. They hug and color each other. 
+  $(".jonas").siblings().css("color", "red");
+
+  // selects sibling div element if found but doesn't color ".knowls"
+  $(".knowls").siblings('div').css("color", "aqua");
+
+  // Immediate catches hold of the element after the "taylor-the-heartless". a clear rebound.
+  $(".taylor-the-showoff").next().css("color", "blue");
+
+
+  // Immediate catches hold of all the elements after itself. Everybody will do attitude.  
+  $(".justin-is-resting").nextAll().css("color", "blueviolet ");
+
+  // bear it until a point then `we are done`.  
+  $(".giving-chances").nextUntil(".brake-up").css("color", "brown");
+
+  // prev(), prevAll() & prevUntil() Methods do exactlt like the above. Going back in time and reminiscing!
+
+  $(".single-hits p").first().css("color", "burlywood ");
+
+
+  $(".will-be-back-soon-guys p").last().css("color", "cadetblue");
+
+
+  // I have the power to randomly pick who ever i wish in the index.
+  $(".song-now-playing").eq(3).css("color", "chartreuse");
+
+
+  // you can filter the elements 
+  $("li").filter(".still-disses").css("background-color", "coral");
+  $(".pals").not(".no-invite").css("background-color", "cornflowerblue ");
+
+
+  // /////////
+
+
+
+  //  $( ".columns" ).find('a').css( "color", "green");
+  //  $( ".columns" ).find('a').each(function(index, el) {
+  //  	  console.log(el)	
+  //  	  var beta = $(this);
+  //  	  console.log(beta);
+
+  //  	  var title = beta.text();
+  //  	  console.log(title)
+  //  });
+
+  //  // console.log($(".box").find('p').text())
+  // /////////
+  // $(".box").filter(function(index) {
+  // 	var data = $(this);
+  // 	console.log(data)
+
+  // 	var title = data.children().text();
+  // 	console.log(title)
+
+  // 	// return something;
+  // });
+
+ });
+
+  // $('div:not(:has(div)) [href*="hello/"]').css( "color", "red");//this works
+
+
