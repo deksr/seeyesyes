d6913a27849d6c0212ddc98bd766220cd8b3207b
devatha k <k.n.devs@gmail.com>
2016-12-14 20:58:54 -0500

    changed folder structure

---
+++ 007-stringAndArray/scripts.js
@@ -0,0 +1,47 @@
+console.log("hi")
+
+var StringOne = "Help me, Obi-Wan Kenobi.";
+var StringTwo = "You're my only hope";
+
+
+string1 = ["axios.get('/jdhsjshdg')", "axios.get('/qeqwyei')", "axios.get('/mnbcamsca')"]
+
+console.log(StringOne.charAt(3))
+
+// *****************
+// concat string
+console.log("Star wars Quote: ".concat(StringOne, StringTwo ))
+//Star wars Quote: Help me, Obi-Wan Kenobi. You're my only hope"
+
+
+// *****************
+// This method determines whether the string contains the character of the given string
+console.log(StringOne.includes('Obi')) //true
+
+
+// *****************
+for (var i = 0; i < string1.length; i++) {
+	console.log(string1[i])
+
+};
+console.log(string1.split("'"))
+
+
+
+   var slicednadcplit = ["axios.get('/hjsdas')","axios.get('/uewes')" ]
+    console.log(axios.all(slicednadcplit))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
